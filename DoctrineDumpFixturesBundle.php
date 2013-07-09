4f71d92d1403a6e631c16675fd03c876e0d21a68
Piotr Plenik <piotr.plenik@gmail.com>
2013-07-09 23:00:09 +0200

    create bundle main class

---
+++ DoctrineDumpFixturesBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Doctrine Dump Fixtures Bundle
+ *
+ * (c) Piotr Plenik <piotr.plenik@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Doctrine\Bundle\DumpFixturesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle.
+ *
+ * @author Piotr Plenik <piotr.plenik@gmail.com>
+ */
+class DoctrineDumpFixturesBundle extends Bundle
+{
+}
