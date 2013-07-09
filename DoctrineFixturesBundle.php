b33c714ef48fa035b15db369d863ab9bf8674034
Piotr Plenik <piotr.plenik@gmail.com>
2013-07-09 23:28:31 +0200

    add missing bundle class

---
+++ DoctrineFixturesBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Doctrine Dump Fixtures Bundle
+ *
+ * (c) Piotr Plenik <piotr.plenik@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TeamLab\Bundle\FixturesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle.
+ *
+ * @author Piotr Plenik <piotr.plenik@gmail.com>
+ */
+class DoctrineFixturesBundle extends Bundle
+{
+}
